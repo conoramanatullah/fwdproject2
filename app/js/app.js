a946786b2402611b1cb799a6139fb5a81e492adf
Conor Amanatullah <Conor Amanatullah>
2016-11-11 11:57:10 -0700

    Made more things

--- app/js/app.js
+++ app/js/app.js
@@ -15,14 +15,18 @@
 
     function( texture ) {
       // Do something with the texture
-      var geometry = new THREE.SphereGeometry(2,64,64 );
+      var geometry = new THREE.SphereGeometry(.1,64,64 );
 
-      var material = new THREE.MeshBasicMaterial( {
+
+      var material = new THREE.MeshPhongMaterial( {
         map: texture,
-        overdraw: 0.5,
+        // overdraw: 0.5,
       } );
 
       var sphere = new THREE.Mesh( geometry, material );
+      sphere.position.x = 5;
+      // sphere.position.y = 5;
+
       scene.add( sphere );
 
     },
@@ -34,6 +38,30 @@
       console.log('Error Loading Texture');
     }
   );
+
+
+  loader.load(
+    '../assets/jupiter.jpg',
+    function ( texture ){
+      var geometry = new THREE.SphereGeometry(4, 64, 64);
+      var material = new THREE.MeshPhongMaterial( {
+        map: texture,
+        // overdraw: 0.5,
+      } );
+
+      var sphere = new THREE.Mesh( geometry, material );
+      // sphere.position.x = 4;
+      scene.add( sphere );
+    },
+    function( xhr ) {
+      console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    } ,
+    function( xhr ) {
+      console.log('Error Loading Texture');
+    }
+
+
+  )
 
 
 
@@ -51,10 +79,35 @@
   // document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 
-  camera.position.z = 3;
+  camera.position.z = 8;
+  camera.position.x = 10;
+  camera.position.y = 10;
 
   var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
   scene.add(light);
+
+  var skyGeo = new THREE.SphereGeometry(100000, 25, 25);
+
+  loader.load(
+    '../assets/jupiter.jpg',
+    function( texture ){
+      var material = new THREE.MeshPhongMaterial({
+        map: texture
+      });
+      var sky = new THREE.Mesh(skyGeo, material);
+      sky.material.side = THREE.BackSide;
+      scene.add(sky);
+    },
+    function( xhr ) {
+      console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    } ,
+    function( xhr ) {
+      console.log('Error Loading Texture');
+    }
+  )
+
+  var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+  scene.add( light );
 
   function render() {
     requestAnimationFrame( render );
