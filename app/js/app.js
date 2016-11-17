e5c428fb0de8b5c8f065fa5d56c34a707b9ac7f7
Conor Amanatullah <Conor Amanatullah>
2016-11-17 10:27:52 -0700

    added jquery and an interface js file

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,21 @@
 
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  var renderer = new THREE.WebGLRenderer();
+  var renderer = new THREE.WebGLRenderer({
+    antialiasing: true
+  });
   var loader = new THREE.TextureLoader();
   var mouseX = 0, mouseY = 0;
   var windowHalfX = window.innerWidth / 2;
   var windowHalfY = window.innerHeight / 2;
   var material;
+  var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+  var skyGeo = new THREE.SphereGeometry(100000, 25, 25);
+  var ambientLight = new THREE.AmbientLight( 0x404040 );
 
-
+  camera.position.z = 8;
+  camera.position.x = 10;
+  camera.position.y = 10;
 
   container = document.getElementById('container');
 
@@ -22,7 +29,7 @@
 
     function( texture ) {
       // Do something with the texture
-      var geometry = new THREE.SphereGeometry(2,64,64 );
+      var geometry = new THREE.SphereGeometry(4,64,64 );
 
 
       var material = new THREE.MeshPhongMaterial( {
@@ -31,7 +38,7 @@
       } );
 
       var sphere = new THREE.Mesh( geometry, material );
-      sphere.position.x = 3;
+      // sphere.position.x = 3000;
       // sphere.position.y = 5;
 
       scene.add( sphere );
@@ -46,45 +53,18 @@
     }
   );
 
-  // document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-
-  camera.position.z = 8;
-  camera.position.x = 10;
-  camera.position.y = 10;
-
-  var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
   scene.add(light);
-
-  var skyGeo = new THREE.SphereGeometry(100000, 25, 25);
-
-  loader.load(
-    '../assets/jupiter.jpg',
-    function( texture ){
-      var material = new THREE.MeshPhongMaterial({
-        map: texture
-      });
-      var sky = new THREE.Mesh(skyGeo, material);
-      sky.material.side = THREE.BackSide;
-      scene.add(sky);
-    },
-    function( xhr ) {
-      console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    } ,
-    function( xhr ) {
-      console.log('Error Loading Texture');
-    }
-  )
-
-  var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-  scene.add( light );
+  scene.add( ambientLight );
 
   function render() {
     requestAnimationFrame( render );
     camera.position.x += ( mouseX - camera.position.x ) * 0.05;
 		camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
 		camera.lookAt( scene.position );
-		scene.rotation.y -= 0.005;
+		// scene.rotation.y -= 0.005;
+    scene.rotation.y += 0.005;
+    scene.position.x = 5;
+    // scene.position.z -= 0.0005
     renderer.render( scene, camera );
   }
   render();
---
+++ app/js/interface.js
@@ -0,0 +1,5 @@
+(function(){
+  console.log('Interface Loaded.');
+
+  
+})();
