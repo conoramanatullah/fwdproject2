5bd1bcde6ce1f00b3ecbf09cedfc6318b1bd21ba
conoramanatullah <conor.amanatullah@gmail.com>
2016-11-17 23:40:31 -0700

    added more planets

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,9 @@
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
   var renderer = new THREE.WebGLRenderer({
-    antialiasing: true
+    antialiasing: true,
   });
+  var maxAnisotropy = renderer.getMaxAnisotropy();
   var loader = new THREE.TextureLoader();
   var mouseX = 0, mouseY = 0;
   var windowHalfX = window.innerWidth / 2;
@@ -14,55 +15,131 @@
   var skyGeo = new THREE.SphereGeometry(100000, 25, 25);
   var ambientLight = new THREE.AmbientLight( 0x404040 );
 
+
+
   camera.position.z = 8;
   camera.position.x = 10;
   camera.position.y = 10;
-
-  container = document.getElementById('container');
-
   renderer.setSize(window.innerWidth, window.innerHeight);
 
+
+  container = document.getElementById('container');
   container.appendChild( renderer.domElement );
 
-  loader.load(
-    '../assets/europa.jpg',
+  function loadEuropa(){
+    console.log('loading Europa');
+    var texture = loader.load(
+      '../assets/europa.jpg',
+      function( texture ) {
 
-    function( texture ) {
-      // Do something with the texture
-      var geometry = new THREE.SphereGeometry(4,120,120 );
+        var geometry = new THREE.SphereGeometry(4,120,120 );
+        var material = new THREE.MeshPhongMaterial( {
+          map: texture,
+        } );
+        var sphere = new THREE.Mesh( geometry, material );
+        sphere.name = 'europa';
+        scene.add( sphere );
+      },
+      function( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+      } ,
+      function( xhr ) {
+        console.log('Error Loading Texture');
+      }
+    );
+
+    texture.anisotropy = maxAnisotropy;
+  };
+  function loadTitan(){
+    console.log('loading Titan');
+    loader.load(
+      '../assets/titan.jpg',
+      function( texture ) {
+        //remove any other scene geometry
+        texture.anisotropy = maxAnisotropy;
+        var geometry = new THREE.SphereGeometry(4,120,120 );
+        var material = new THREE.MeshPhongMaterial( {
+          map: texture,
+        } );
+        var sphere = new THREE.Mesh( geometry, material );
+        sphere.name = 'titan';
+        scene.add( sphere );
+      },
+      function( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+      } ,
+      function( xhr ) {
+        console.log('Error Loading Texture');
+      }
+    );
 
 
-      var material = new THREE.MeshPhongMaterial( {
-        map: texture,
-        // overdraw: 0.5,
-      } );
+  };
+  function loadJupiter(){
+    console.log('loading Jupiter');
+    loader.load(
+      '../assets/jupiter.jpg',
+      function( texture ) {
 
-      var sphere = new THREE.Mesh( geometry, material );
-      // sphere.position.x = 3000;
-      // sphere.position.y = 5;
+        var geometry = new THREE.SphereGeometry(4,120,120 );
+        var material = new THREE.MeshPhongMaterial( {
+          map: texture,
+        } );
+        var sphere = new THREE.Mesh( geometry, material );
+        sphere.name = 'jupiter';
+        scene.add( sphere );
+      },
+      function( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+      } ,
+      function( xhr ) {
+        console.log('Error Loading Texture');
+      }
+    );
+  };
 
-      scene.add( sphere );
-
-    },
-
-    function( xhr ) {
-      console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    } ,
-    function( xhr ) {
-      console.log('Error Loading Texture');
-    }
-  );
-
+  function removeEntity(object) {
+    var selectedObject = scene.getObjectByName(object.name);
+    scene.remove( selectedObject );
+    // animate();
+  };
   scene.add(light);
   scene.add( ambientLight );
+
+  $('#europa-button').on('click', function(){
+    scene.remove(scene.getObjectByName('titan'));
+    scene.remove(scene.getObjectByName('jupiter'));
+
+    loadEuropa();
+
+  });
+
+  $('#titan-button').on('click', function(){
+    console.log('Loading Titan');
+    scene.remove(scene.getObjectByName('europa'));
+    scene.remove(scene.getObjectByName('jupiter'));
+
+    loadTitan();
+
+  });
+
+  $('#jupiter-button').on('click', function(){
+    scene.remove(scene.getObjectByName('titan'));
+    scene.remove(scene.getObjectByName('europa'));
+
+    loadJupiter();
+
+  });
+
+
   container.addEventListener('click', function(){
     if(camera.position.z == 1){
       camera.position.z = 8;
     } else {
       camera.position.z = 1;
     }
-
   });
+
   function render() {
     requestAnimationFrame( render );
     camera.position.x += ( mouseX - camera.position.x ) * 0.05;
@@ -75,5 +152,6 @@
     // scene.position.z -= 0.0005
     renderer.render( scene, camera );
   }
+  loadEuropa();
   render();
 })();
--- app/js/interface.js
+++ app/js/interface.js
@@ -2,8 +2,22 @@
   console.log('Interface Loaded.');
   //Animate stats loading in
   $('#interface-content').show();
-  // Load First Quiz page using jquery
-  $('#interface-panel-1').load('views/panel1.html');
+
+  // Load Europa stats
+  $('#interface-content').load('views/europa-stats.html');
+
+  $('#europa-button').on('click', function(){
+    $('#interface-content').load('views/europa-stats.html');
+
+  });
+  $('#titan-button').on('click', function(){
+    $('#interface-content').load('views/titan-stats.html');
+
+  });
+  $('#jupiter-button').on('click', function(){
+    $('#interface-content').load('views/jupiter-stats.html');
+
+  });
 
   // set hover functionality
   $('.list-element').on('mouseenter', function(){
