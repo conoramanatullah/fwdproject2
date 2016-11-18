13fa3c0765fa957025f4cf087a1f53426a7543a6
conoramanatullah <conor.amanatullah@gmail.com>
2016-11-17 20:06:08 -0700

    milestone 2 complete

--- app/js/interface.js
+++ app/js/interface.js
@@ -1,5 +1,13 @@
 (function(){
   console.log('Interface Loaded.');
+  //Animate stats loading in
+  $('#interface-content').show();
+  // Load First Quiz page using jquery
+  $('#interface-panel-1').load('views/panel1.html');
 
-  
+  // set hover functionality
+  $('.list-element').on('mouseenter', function(){
+
+  });
+
 })();
